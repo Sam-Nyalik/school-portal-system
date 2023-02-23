81db6cb0d4c12cd9129634f0730a1c1722675742
joshuaobare <jobare.obare@gmail.com>
2023-02-23 11:35:19 +0300

    created lecturers table

--- school-portal-system.sql
+++ school-portal-system.sql
@@ -1,7 +1,7 @@
 create database school_portal_system;
 use school_portal_system;
 
-create table students(
+create table students (
 studentID integer auto_increment primary key,
 first_name varchar(10) not null,
 last_name varchar(10) not null,
@@ -20,3 +20,13 @@ unit_title varchar(50) not null,
 lecturerID integer not null
 );
 
+create table lecturers (
+lecturerID integer primary key auto_increment,
+first_name varchar(10) not null,
+last_name varchar(10) not null,
+email varchar(50) not null,
+phone_number varchar(50) not null,
+password varchar(50) not null,
+date_of_birth date not null,
+gender varchar(6) not null
+);
