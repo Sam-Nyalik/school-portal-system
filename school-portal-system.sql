9bf13ea2cc804f38133c077a2456a01ee885d684
joshuaobare <jobare.obare@gmail.com>
2023-02-23 11:25:33 +0300

    updated students table and created units table

--- school-portal-system.sql
+++ school-portal-system.sql
@@ -8,5 +8,15 @@ last_name varchar(10) not null,
 email varchar(50) not null,
 phone_number varchar(50) not null,
 password varchar(50) not null,
-enrol_date date not null
+enrol_date date not null,
+date_of_birth date not null,
+gender varchar(6) not null,
+current_year integer
 );
+
+create table units (
+unitID varchar(10) primary key,
+unit_title varchar(50) not null,
+lecturerID integer not null
+);
+
