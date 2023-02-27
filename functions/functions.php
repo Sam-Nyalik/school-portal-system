f546f7e9d415d52517e1d92c0084544bb997db57
Sam-Nyalik <samnyalik@gmail.com>
2023-02-27 22:42:48 +0300

    Database connection script update

--- functions/functions.php
+++ functions/functions.php
@@ -1,14 +1,17 @@
-<!-- Database Connection -->
+<?php 
+// Database Connection 
 function db_connect(){
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASSWORD = '';
-    $DATABASE_NAME = '';
+    $DATABASE_NAME = 'school_portal_system';
 
     try {
         return new PDO("mysql:host=" .$DATABASE_HOST . "; dbname=" . $DATABASE_NAME . ";charset=utf-8", $DATABASE_USER, $DATABASE_PASSWORD);
     } catch (PDOException $exception){
-        <!-- Stop the script and generate an error incase there's a problem -->
+        // Stop the script and generate an error incase there's a problem 
         exit("Connection to the database was unsuccessful" . $exception->getMessage());
     }
 }
+
+?>
