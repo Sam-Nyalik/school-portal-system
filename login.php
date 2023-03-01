272973fc1fc6664ac988747089a888010bdba6a4
joshuaobare <jobare.obare@gmail.com>
2023-03-01 19:43:07 +0300

    created basic login form

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>Login Page</h1>
+    <form method="post" class="login-form">
+        <div class="login-form-item">
+            <label for="">Email:</label>
+            <input type="email">
+        </div>
+        <div class="login-form-item">
+            <label for="">Password</label>
+            <input type="text">
+        </div>
+        <button class="login-button">Log In</button>
+    </form>
+</body>
+</html>
