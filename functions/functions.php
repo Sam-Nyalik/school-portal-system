36df003d3f4b2bb0505128a9e32154e08489bdd0
Sam-Nyalik <samnyalik@gmail.com>
2023-03-01 12:33:54 +0300

    Quick update - Added the header template

--- functions/functions.php
+++ functions/functions.php
@@ -1,17 +1,35 @@
 <?php 
 // Database Connection 
 function db_connect(){
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASSWORD = '';
-    $DATABASE_NAME = 'school_portal_system';
+    $DATABASE_HOST = "localhost";
+    $DATABASE_USER = "root";
+    $DATABASE_PASSWORD = "";
+    $DATABASE_NAME = "school_portal_system";
 
     try {
-        return new PDO("mysql:host=" .$DATABASE_HOST . "; dbname=" . $DATABASE_NAME . ";charset=utf-8", $DATABASE_USER, $DATABASE_PASSWORD);
+        return new PDO("mysql:host=" . $DATABASE_HOST . ";dbname=" . $DATABASE_NAME . ";charset=utf8", $DATABASE_USER, $DATABASE_PASSWORD);
     } catch (PDOException $exception){
         // Stop the script and generate an error incase there's a problem 
         exit("Connection to the database was unsuccessful" . $exception->getMessage());
     }
+}
+
+// Header Template
+function header_template($page_title) 
+{
+    $element = "
+        <!DOCTYPE html>
+        <html lang=\"en\">
+        <head>
+        <title>$page_title</title>
+        <meta charset=\"utf-8\">
+        <meta name=\"viewport\" content=\"width=device-width\">
+        <meta name=\"author\" content=\"Bryan Kiragu; Joshua obare; Samson Nyalik\">
+        <link rel=\"stylesheet\" href=\"css/styles.css\">
+        <link rel=\"stylesheet\" href=\"css/bootstrap.min.css\">
+    ";
+    
+    echo $element;
 }
 
 ?>
