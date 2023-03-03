b6cfcef7bb292220fee46faa4e3c67d77c3e8ccc
joshuaobare <jobare.obare@gmail.com>
2023-03-03 19:15:17 +0300

    removed previous code and added comments to registration.php

--- registration.php
+++ registration.php
@@ -8,109 +8,17 @@ $pdo = db_connect();
 // Define variables and assign them empty values
 $firstName = $lastName = $emailAddress = $phoneNumber = $password = $confirmPassword = $gender = $role = $dateOfBirth = $yearOfStudy = $course = $department = "";
 $firstName_error = $lastName_error = $emailAddress_error = $phoneNumber_error = $password_error = $confirmPassword_error = $gender_error = $role_error = $dateOfBirth_error = $yearOfStudy_error = $course_error = $department_error = "";
-/*
-// ADMIN REGISTRATION
-// Process form data when the form has been submitted
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['role'] == 'admin') {
-    // there might be a way to use a loop for this
-    /*
-    // Validate firstName
-    if (empty(trim($_POST['firstName']))) {
-        $firstName_error = "Field is required!";
-    } else {
-        $firstName = trim($_POST['firstName']);
-    }
 
-    // Validate lastName
-    if (empty(trim($_POST['lastName']))) {
-        $lastName_error = "Field si required!";
-    } else {
-        $lastName = trim($_POST['lastName']);
-    }
-
-    // Validate emailAddress
-    if (empty(trim($_POST['emailAddress']))) {
-        $emailAddress_error = "Field is required!";
-    } else {
-        $emailAddress = trim($_POST['emailAddress']);
-    }
-
-    // Validate phoneNumber
-    if (empty(trim($_POST['phoneNumber']))) {
-        $phoneNumber_error = "Field is required!";
-    } else {
-        $phoneNumber = trim($_POST['phoneNumber']);
-    }
-
-    // Validate Password
-    if (empty(trim($_POST['password']))) {
-        $password_error = "Field is required";
-    } elseif (strlen(trim($_POST['password'])) < 8) {
-        $password_error = "Passwords must contain more than 8 characters!";
-    } else {
-        $password = trim($_POST['password']);
-    }
-
-    // Validate confirmPassword
-    if (empty(trim($_POST['confirmPassword']))) {
-        $confirmPassword_error = "Field is required!";
-    } else {
-        $confirmPassword = trim($_POST['confirmPassword']);
-
-        // Check if the passwords match
-        if (empty($password_error) && ($password !== $confirmPassword)) {
-            $confirmPassword_error = "Passwords do not match!";
-        }
-    }
-
-    // Validate gender
-    if (empty(trim($_POST['gender']))) {
-        $gender_error = "Field is required!";
-    } else {
-        $gender = trim($_POST['gender']);
-    } */
-
-    // Check for errors before dealing with the database
-    /*
-    if (empty($firstName_error) && empty($lastName_error) && empty($emailAddress_error) && empty($phoneNumber_error) && empty($password_error) && empty($confirmPassword_error) && empty($gender_error)) {
-        // Prepare an INSERT statement
-        $admin_insert = "INSERT INTO users(first_name, last_name, email, phone_number, password, gender, roleID) VALUES(:firstName, :lastName, :emailAddress, :phoneNumber, :password, :gender, :roleId)";
-        if ($stmt = $database_connection->prepare($admin_insert)) {
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":firstName", $param_firstName, PDO::PARAM_STR);
-            $stmt->bindParam(":lastName", $param_lastName, PDO::PARAM_STR);
-            $stmt->bindParam(":emailAddress", $param_emailAddress, PDO::PARAM_STR);
-            $stmt->bindParam(":phoneNumber", $param_phoneNumber, PDO::PARAM_STR);
-            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
-            $stmt->bindParam(":gender", $param_gender, PDO::PARAM_STR);
-            $stmt->bindParam(":roleId", $param_roleId, PDO::PARAM_INT);
-
-            // Set parameters
-            $param_firstName = $firstName;
-            $param_lastName = $lastName;
-            $param_emailAddress = $emailAddress;
-            $param_phoneNumber = $phoneNumber;
-            $param_password = password_hash($password, PASSWORD_DEFAULT);
-            $param_gender = $gender;
-            $param_roleId = 1;
-
-            // Attemept to execute
-            if($stmt->execute()){
-                // Redirect admin to the login page
-                header("location: admin_login.php");
-            } else {
-                echo("There was an error, please try again!");
-            }
-        }
-    }
-}
-*/
+// After client-side data validation and form submission, a check is made for whether relevant data exists in the $_POST array 
 if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['emailAddress']) && isset($_POST['phoneNumber']) && isset($_POST['password']) && isset($_POST['confirmPassword']) && isset($_POST['gender']) && isset($_POST['role'])){
 
+    // This try-catch block inserts data into the users table by default
     try {
     // Prepare an INSERT statement
     $admin_insert = "INSERT INTO users(first_name, last_name, email, phone_number, password, gender, roleID) VALUES(:firstName, :lastName, :emailAddress, :phoneNumber, :password, :gender, :roleId)";
     $stmt = $pdo->prepare($admin_insert);
+
+    // The placeholders from the INSERT statement are replaced with actual values from the $_POST array before being executed
     $stmt->execute(array(
         ":firstName" => $_POST['firstName'],
         ":lastName" => $_POST['lastName'],
@@ -119,14 +27,14 @@ if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['ema
         ":password" => password_hash($_POST['password'], PASSWORD_DEFAULT),
         ":gender" => $_POST['gender'],
         ":roleId" => $_POST['role']
-    ));
-    
-    
+    ));    
+      // an error message will be echoed
     } catch (Exception $e){
         echo "Error<br>" .$e->getMessage();
     }
     $user_id = $pdo->lastInsertId();
 
+    // This if block is executed if the student role is chosen
     if(isset($_POST['dob']) && isset($_POST['yearOfStudy']) && isset($_POST['course']) ){
         
         try{
@@ -144,6 +52,7 @@ if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['ema
         }
     }
 
+    // This if block is executed if the lecturer role is chosen 
     if(isset($_POST['department'])) {
         try{
             $lecturer_insert = "INSERT INTO lecturers(userID , departmentID) values(:userID , :department)";
@@ -158,131 +67,7 @@ if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['ema
     }
 
 }
-/*
-// STUDENT REGISTRATION
-// Process form data when the form has been submitted
-if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['role'] == 'student'){
-    // Validate firstName
-    if (empty(trim($_POST['firstName']))) {
-        $firstName_error = "Field is required!";
-    } else {
-        $firstName = trim($_POST['firstName']);
-    }
 
-    // Validate lastName
-    if (empty(trim($_POST['lastName']))) {
-        $lastName_error = "Field si required!";
-    } else {
-        $lastName = trim($_POST['lastName']);
-    }
-
-    // Validate emailAddress
-    if (empty(trim($_POST['emailAddress']))) {
-        $emailAddress_error = "Field is required!";
-    } else {
-        $emailAddress = trim($_POST['emailAddress']);
-    }
-
-    // Validate phoneNumber
-    if (empty(trim($_POST['phoneNumber']))) {
-        $phoneNumber_error = "Field is required!";
-    } else {
-        $phoneNumber = trim($_POST['phoneNumber']);
-    }
-
-    // Validate Password
-    if (empty(trim($_POST['password']))) {
-        $password_error = "Field is required";
-    } elseif (strlen(trim($_POST['password'])) < 8) {
-        $password_error = "Passwords must contain more than 8 characters!";
-    } else {
-        $password = trim($_POST['password']);
-    }
-
-    // Validate confirmPassword
-    if (empty(trim($_POST['confirmPassword']))) {
-        $confirmPassword_error = "Field is required!";
-    } else {
-        $confirmPassword = trim($_POST['confirmPassword']);
-
-        // Check if the passwords match
-        if (empty($password_error) && ($password !== $confirmPassword)) {
-            $confirmPassword_error = "Passwords do not match!";
-        }
-    }
-
-    // Validate gender
-    if (empty(trim($_POST['gender']))) {
-        $gender_error = "Field is required!";
-    } else {
-        $gender = trim($_POST['gender']);
-    }
-
-    // Validate D.O.B
-    if(empty(trim($_POST['dob']))){
-        $dateOfBirth_error = "Field is required!";
-    } else {
-        $dateOfBirth = trim($_POST['dob']);
-    }
-
-    // Validate Year of study
-    if(empty(trim($_POST['yearOfStudy']))){
-        $yearOfStudy_error = "Field is required!";
-    } else {
-        $yearOfStudy = trim($_POST['yearOfStudy']);
-    }
-
-    // Validate course
-    if(empty(trim($_POST['course']))){
-        $course_error = "Field is required!";
-    } else {
-        $course = trim($_POST['course']);
-    }
-
-    // Check for errors before dealing with the database
-    if (empty($firstName_error) && empty($lastName_error) && empty($emailAddress_error) && empty($phoneNumber_error) && empty($password_error) && empty($confirmPassword_error) && empty($gender_error) && empty($dateOfBirth_error) && empty($yearOfStudy_error) && empty($course_error)) {
-        // Prepare an INSERT statement
-        $student_insert = "INSERT INTO users(first_name, last_name, email, phone_number, password, gender, roleID) VALUES(:firstName, :lastName, :emailAddress, :phoneNumber, :password, :gender, :roleId) INTO students(date_of_birth, year, userID, courseID) VALUES(:dob, :yearOfStudy, :userID, :courseID)";
-        if ($stmt = $database_connection->prepare($student_insert)) {
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":firstName", $param_firstName, PDO::PARAM_STR);
-            $stmt->bindParam(":lastName", $param_lastName, PDO::PARAM_STR);
-            $stmt->bindParam(":emailAddress", $param_emailAddress, PDO::PARAM_STR);
-            $stmt->bindParam(":phoneNumber", $param_phoneNumber,
-                PDO::PARAM_STR
-            );
-            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
-            $stmt->bindParam(":gender", $param_gender, PDO::PARAM_STR);
-            $stmt->bindParam(":roleId", $param_roleId, PDO::PARAM_INT);
-            $stmt->bindParam(":dob", $param_dob, PDO::PARAM_STR);
-            $stmt->bindParam(":yearOfStudy", $param_yearOfStudy, PDO::PARAM_STR);
-            $stmt->bindParam(":userID", $param_userID, PDO::PARAM_INT);
-            $stmt->bindParam(":courseID", $param_courseID, PDO::PARAM_INT);
-
-            // Set parameters
-            $param_firstName = $firstName;
-            $param_lastName = $lastName;
-            $param_emailAddress = $emailAddress;
-            $param_phoneNumber = $phoneNumber;
-            $param_password = password_hash($password, PASSWORD_DEFAULT);
-            $param_gender = $gender;
-            $param_roleId = 2;
-            $param_dob = $dateOfBirth;
-            $param_yearOfStudy = $yearOfStudy;
-            $param_userID ="" ;
-            $param_courseID = "";
-
-            // Attemept to execute
-            if ($stmt->execute()) {
-                // Redirect student to the login page
-                header("location: student_login.php");
-            } else {
-                echo ("There was an error, please try again!");
-            }
-        }
-    }
-}
-*/
 ?>
 
 <!-- Header Template -->
