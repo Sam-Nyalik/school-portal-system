16e5c96b50e7d81a3e421f800c0c80231bd2abfe
joshuaobare <jobare.obare@gmail.com>
2023-03-03 19:20:00 +0300

    added comments to registration.js

--- registration.js
+++ registration.js
@@ -9,19 +9,23 @@ const role = document.getElementById("role");
 const lecturerInput = document.querySelector(".lecturerInput");
 const studentsInputs = document.querySelectorAll(".studentsInputs")
 
+// an event listener is added to the role drop down menu
 role.addEventListener("change", (e) => {
-  console.log("role changed")
-  console.log(role.value)
+  // if lecturer is selected, the students' form inputs are hidden, while the lecturers' is shown
   if (role.value === "3") {
     lecturerInput.classList.remove("hide");
     studentsInputs.forEach((item) => {
       item.classList.add("hide");
     });
+
+    // if student is selected, the lecturers' form inputs are hidden, while the students' are shown
   } else if (role.value === "2") {
     studentsInputs.forEach((item) => {
       item.classList.remove("hide");
     });
     lecturerInput.classList.add("hide");
+
+    // if admin is selected, all the other inputs are hidden
   } else {
     lecturerInput.classList.add("hide");
     studentsInputs.forEach((item) => {
@@ -30,4 +34,3 @@ role.addEventListener("change", (e) => {
   }
 });
 
-console.log("loaded")
