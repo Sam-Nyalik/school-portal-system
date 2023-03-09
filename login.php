dd51587f308f052172f474536ed3fadd048f12d6
joshuaobare <jobare.obare@gmail.com>
2023-03-09 13:54:03 +0300

    renamed login stylesheet

--- login.php
+++ login.php
@@ -47,7 +47,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
     <script src="login.js" defer></script>
     <title>Login Page</title>
 </head>
